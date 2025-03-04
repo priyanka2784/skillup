388c924fe83639c425e196f8d02ad93f177dea9e
priyanka2784 <priyankasodhi617@gmail.com>
2025-03-04 11:05:44 +0530

    updated

--- src/config/COLOR.js
+++ src/config/COLOR.js
@@ -1,6 +1,6 @@
 const COLOR = {
-  basecolor: "#70AE6E",
-  basecolorsecond: "#FEFEFF",
+  basecolor: "#002500",
+  basecolorsecond: "#F0F7F4",
   secondarycolor: "rgb(139, 134, 134)",
   blackcolor: "#000000",
   bordercolor: "#dadada",
--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import CustomInput from "../../COMPONENTS/CustomInput/CustomInput.js";
+import COLOR from "../../config/COLOR.js";
+import "./styles.css";
+import { FaUserAlt } from "react-icons/fa";
+import { FaKey } from "react-icons/fa";
 function RegisterPage() {
+  const [email, setEmail] = useState("@gmail.com");
+  const [username, setusername] = useState("enterusername");
+  const [password, setPassword] = useState("");
   return (
-    <div>
-      <h1>registerpage</h1>
+    <div className="mainbaseconatiner">
+      <h1 className="headingcontainer">
+        <span style={{ color: COLOR.basecolor }}>S</span>Kill-
+        <span style={{ color: COLOR.secondarycolor }}>U</span>p
+      </h1>
+      <div className="loginbasecontainer">
+        <div className="ImageContainer">
+          <img alt="elearning" />
+        </div>
+
+        <div className="textbasecontainer"></div>
+
+        <div className="usernamecontainer">
+          <h1>WELCOME</h1>
+          <h2>{`Register Here! ${email}`}</h2>
+          <CustomInput
+            type={"email"}
+            placeholder={"EnterEmail"}
+            Icon={FaUserAlt}
+            inputValue={email}
+            onChangeText={(e) => {
+              alert(e.target.value);
+              setEmail(e.target.value);
+            }}
+          />
+          <CustomInput
+            type="password"
+            placeholder="Enter password"
+            Icon={FaKey}
+            isSecureEntry={"true"}
+            inputValue={password}
+            onChangeText={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <div className="buttoncontainer">
+            <button>Register</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
