03f98475ba33179a9abac91d557bb6805a3217cf
priyanka2784 <priyankasodhi617@gmail.com>
2025-03-13 14:29:07 +0530

    updated

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,19 +2,20 @@ import "./styles.css";
 function HomePage() {
   return (
     <div className="basecontainer">
+      <div className="navbar"></div>
       <div className="basecontainer1">
-        <div className="contentbasecontainer1">
-          <h1>skillup🎚️</h1>
-        </div>
-        <div className="imagebasecontainer1"></div>
-      </div>
-      <div className="secondbasecontainer2"></div>
-      <div className="basecontainer3">
-        <div className="imagebasecontainer3">
-          <img src={require("../../Assets/images/elearning.png")} />
+        <div className="textbasecontainer1">
+          <div className="buttonconatiner1"></div>
         </div>
       </div>
-      <div className="basecontainer3"></div>
+      <div className="imagebasecontainer1"></div>
+      <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
+      <div className=""> </div>
+      <div className=""></div>
+      <div className=""></div>
     </div>
   );
 }
--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,17 +1,70 @@
 import React, { useState } from "react";
 import "./styles.css";
-import { auth } from "../../firebase";
+import { auth, database } from "../../firebase";
 import ASSETS from "../../Assets";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import CustomButton from "../../COMPONENTS/CustomButton/customButton";
 import COLOR from "../../config/COLOR";
+import { Form, Navigate, useNavigate } from "react-router-dom";
+import { Database, ref, set } from "firebase/database";
 
 function RegisterPage() {
+  const [Name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [ConfirmPassword, setconfirmPassword] = useState("");
+  const [buttonText, setbuttonText] = useState("");
+  const navigate = useNavigate();
+
+  const saveUserDetail = (data) => {
+    set(ref(database, `contentcreator/${data.uid}`), data);
+    set(ref(database, `contentreader/${data.uid}`), data);
+    set(ref(database, `both/${data.uid}`), data);
+
+    navigate("/login");
+  };
 
   const handleRegister = async () => {
-    await createUserWithEmailAndPassword(auth, email, password);
+    try {
+      if (
+        Name == "" ||
+        email == "" ||
+        password == "" ||
+        ConfirmPassword == ""
+      ) {
+        alert("fill it first");
+      } else if (password !== ConfirmPassword) {
+        alert("error not match");
+      } else {
+        setbuttonText("please wait!");
+        const response = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        setbuttonText("Register");
+        if (response.user.uid) {
+          const userData = {
+            uid: response.user.uid,
+            email: response.user.email,
+            Name: Name,
+          };
+          saveUserDetail(userData);
+        } else {
+          alert("failed to register");
+          setEmail("");
+          setPassword("");
+          setconfirmPassword("");
+          setName("");
+        }
+      }
+    } catch (err) {
+      setName("");
+      setEmail("");
+      setPassword("");
+      setconfirmPassword("");
+      alert(err);
+    }
   };
   return (
     <div
@@ -34,7 +87,13 @@ function RegisterPage() {
                   </div>
                 </td>
                 <td>
-                  <input type="text" placeholder="Name" required />
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    value={Name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
                 </td>
               </tr>
               <tr>
@@ -77,6 +136,8 @@ function RegisterPage() {
                   <input
                     type="password"
                     placeholder="Confirm Password"
+                    value={ConfirmPassword}
+                    onChange={(e) => setconfirmPassword(e.target.value)}
                     required
                   />
                 </td>
@@ -106,7 +167,7 @@ function RegisterPage() {
           <CustomButton
             backgroundColor={COLOR.basecolorsecond}
             color={COLOR.blackcolor}
-            title={"register"}
+            title={buttonText}
             onClick={handleRegister}
           />
         </div>
@@ -114,4 +175,5 @@ function RegisterPage() {
     </div>
   );
 }
+
 export default RegisterPage;
